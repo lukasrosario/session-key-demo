2a4573b7a2f8f45fc9d7da79244ab8a9589aee96
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2024-06-25 16:58:40 +0100

    add key creation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { useAccount, useConnect, useDisconnect, useWalletClient } from "wagmi";
 import { startPasskeyRegistration } from "./util/startPasskeyRegistration";
 import { useState } from "react";
 import { Hex, concat, hexToBytes, stringToHex } from "viem";
-import { bundlerClient, getUserOpHashForMint } from "./util/getUserOpHashForMint";
+import {
+  bundlerClient,
+  getUserOpHashForMint,
+} from "./util/getUserOpHashForMint";
 import { truncateMiddle } from "./util/truncateMiddle";
 import { getAuthOptions } from "./util/getAuthOptions";
 import { signUserOp } from "./util/signUserOp";
@@ -18,12 +21,59 @@ function App() {
   const [publicKey, setPublicKey] = useState<Hex | undefined>();
   const [credentialID, setCredentialID] = useState<string | undefined>();
   const [address, setAddress] = useState<Hex | undefined>();
-  const [userOpHash, setUserOpHash] = useState<Hex>('0x');
-  const [submitted, setSubmitted] = useState(false)
-  const {data: userOpReceipt} = useQuery({queryKey: ['opReceipt', userOpHash], queryFn: () => bundlerClient.getUserOperationReceipt({hash: userOpHash}), enabled: userOpHash !== '0x', refetchInterval: 1000})
+  const [userOpHash, setUserOpHash] = useState<Hex>("0x");
+  const [submitted, setSubmitted] = useState(false);
+  const { data: userOpReceipt } = useQuery({
+    queryKey: ["opReceipt", userOpHash],
+    queryFn: () => bundlerClient.getUserOperationReceipt({ hash: userOpHash }),
+    enabled: userOpHash !== "0x",
+    refetchInterval: 1000,
+  });
 
   const createSession = async () => {
-    await connectors[0].connect({requests: [{message: 'Sign in'}]})
+    const { publicKey: newSigner, credentialID: newCredentialId } =
+      await startPasskeyRegistration();
+    const wallet = connectors[0];
+    await wallet.connect({
+      requests: [
+        { message: "Sign in" },
+        {
+          permissions: {
+            expiry: 1577840461,
+            signer: {
+              type: "key",
+              data: {
+                id: newSigner,
+              },
+            },
+            permissions: [
+              {
+                type: "compliance-function",
+                data: {},
+                policies: [
+                  {
+                    type: "native-token-transfer",
+                    data: {
+                      value: "0x100",
+                    },
+                  },
+                  {
+                    type: "contract-call",
+                    data: {
+                      address: "0x...",
+                      calls: [
+                        "function sessionCall(bytes calldata data) external",
+                      ],
+                    },
+                  },
+                ],
+                required: true,
+              },
+            ],
+          },
+        },
+      ],
+    });
     // if (walletClient) {
     //   const { publicKey: newSigner, credentialID: newCredentialId } =
     //     await startPasskeyRegistration();
@@ -86,23 +136,23 @@ function App() {
 
   const mint = async () => {
     if (address && credentialID) {
-      setSubmitted(true)
-      const {hash, userOp} = await getUserOpHashForMint(address);
-      const options = await getAuthOptions(hash, credentialID)
-      const signature = await signUserOp(options, address)
-      const userOpToSubmit = {...userOp, signature}
-      const submittedUserOpHash = await bundlerClient.sendUserOperation({userOperation: userOpToSubmit})
-      setUserOpHash(submittedUserOpHash)
-      setSubmitted(false)
+      setSubmitted(true);
+      const { hash, userOp } = await getUserOpHashForMint(address);
+      const options = await getAuthOptions(hash, credentialID);
+      const signature = await signUserOp(options, address);
+      const userOpToSubmit = { ...userOp, signature };
+      const submittedUserOpHash = await bundlerClient.sendUserOperation({
+        userOperation: userOpToSubmit,
+      });
+      setUserOpHash(submittedUserOpHash);
+      setSubmitted(false);
     }
   };
 
   return (
     <div className="bg-blue-700 h-screen w-screen flex flex-col items-center justify-center text-white relative">
       <div className="absolute top-4 right-4">
-        {address && (
-          <span className="text-lg">{truncateMiddle(address)}</span>
-        )}
+        {address && <span className="text-lg">{truncateMiddle(address)}</span>}
         {!address && (
           <button
             className="bg-white text-black p-2 rounded-lg w-36 text-lg"
@@ -115,27 +165,27 @@ function App() {
       </div>
 
       <div className="div flex flex-col items-center justify-center space-y-8">
-        {!address && (<h2 className="text-xl">Session key demo</h2>)}
+        {!address && <h2 className="text-xl">Session key demo</h2>}
         {address && (
           <>
-          <button
-            className="bg-white text-black p-2 rounded-lg w-36 text-lg disabled:bg-gray-400"
-            type="button"
-            onClick={mint}
-            disabled={submitted}
-          >
-            Mint!
-          </button>
-          {userOpReceipt?.receipt.transactionHash && (
-            <a
-              href={`https://basescan.org/tx/${userOpReceipt.receipt.transactionHash}`}
-              target="_blank"
-              rel="noreferrer"
-              className="text-text-white"
+            <button
+              className="bg-white text-black p-2 rounded-lg w-36 text-lg disabled:bg-gray-400"
+              type="button"
+              onClick={mint}
+              disabled={submitted}
             >
-              View transaction
-            </a>
-          )}
+              Mint!
+            </button>
+            {userOpReceipt?.receipt.transactionHash && (
+              <a
+                href={`https://basescan.org/tx/${userOpReceipt.receipt.transactionHash}`}
+                target="_blank"
+                rel="noreferrer"
+                className="text-text-white"
+              >
+                View transaction
+              </a>
+            )}
           </>
         )}
       </div>
